599b6452e1c0ef210084e08bab41b041566b5e70
jinunn <372138750@qq.com>
2021-06-17 23:03:54 +0800

    feat(*): 修改会员服务配置文件，注册至nacos

--- mall-member/src/main/java/com/jinunn/mall/member/MallMemberApplication.java
+++ mall-member/src/main/java/com/jinunn/mall/member/MallMemberApplication.java
@@ -1,9 +1,16 @@
 package com.jinunn.mall.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @author jinunn
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.jinunn.mall.member.dao")
 public class MallMemberApplication {
 
     public static void main(String[] args) {
