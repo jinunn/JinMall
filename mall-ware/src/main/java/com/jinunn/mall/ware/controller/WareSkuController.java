33adc89d6c42d3ffcf3df42bddd745addc98ef2f
jinunn <372138750@qq.com>
2021-06-20 15:14:15 +0800

    feat(*): 商品库存列表查询

--- mall-ware/src/main/java/com/jinunn/mall/ware/controller/WareSkuController.java
+++ mall-ware/src/main/java/com/jinunn/mall/ware/controller/WareSkuController.java
@@ -36,7 +36,6 @@ public class WareSkuController {
     @RequestMapping("/list")
     public R list(@RequestParam Map<String, Object> params){
         PageUtils page = wareSkuService.queryPage(params);
-
         return R.ok().put("page", page);
     }
 
--- mall-ware/src/main/java/com/jinunn/mall/ware/service/impl/WareSkuServiceImpl.java
+++ mall-ware/src/main/java/com/jinunn/mall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,5 +1,7 @@
 package com.jinunn.mall.ware.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -18,9 +20,17 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        LambdaQueryWrapper<WareSkuEntity> wrapper = new QueryWrapper<WareSkuEntity>().lambda();
+        String skuId = (String) params.get("skuId");
+        String wareId = (String) params.get("wareId");
+        if (StringUtils.isNotBlank(skuId)){
+            wrapper.eq(WareSkuEntity::getSkuId,skuId);
+        }
+        if (StringUtils.isNotBlank(wareId)){
+            wrapper.eq(WareSkuEntity::getWareId,wareId);
+        }
         IPage<WareSkuEntity> page = this.page(
-                new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                new Query<WareSkuEntity>().getPage(params),wrapper
         );
 
         return new PageUtils(page);
